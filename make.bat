8368f4a05766d6c620b0118a997cc184c5470ef0
Denchitos <denisthecool@mail.ru>
2026-06-18 12:54:09 +0300

    Update make.bat file

--- make.bat
+++ make.bat
@@ -1,4 +1,2 @@
 @echo off
-del lang.exe
-g++ lang.cpp source/init.cpp source/expr.cpp source/classes.cpp -std=c++20 -o lang.exe
-lang.exe
+g++ lang.cpp source/init.cpp source/expr.cpp source/classes.cpp source/executors.cpp source/frame.cpp source/interpreter.cpp -std=c++20 -o lang.exe
--- source/interpreter.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-del int.exe
-g++ lang.cpp executors.cpp frame.cpp classes.cpp init.cpp expr.cpp interpreter.cpp -std=c++20 -o int.exe
-int.exe
